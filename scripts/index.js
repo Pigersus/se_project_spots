074bcb4524cbec419a41315db4ade1d4c41ff260
JP Hayes <jpeaster74@gmail.com>
2025-03-07 00:10:17 -0800

    Added and connected index script

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+let initialCards = [];
+
+let obj1 = {
+  name: "Val Thorens",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+};
+
+let obj2 = {
+  name: "Restaurant terrace",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+};
+
+let obj3 = {
+  name: "An outdoor cafe",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+};
+
+let obj4 = {
+  name: "A very long bridge, over the forest and through the trees",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+};
+
+let obj5 = {
+  name: "Tunnel with morning light",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+};
+
+let obj6 = {
+  name: "Mountain house",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+};
+
+initialCards.push(obj1);
+initialCards.push(obj2);
+initialCards.push(obj3);
+initialCards.push(obj4);
+initialCards.push(obj5);
+initialCards.push(obj6);
